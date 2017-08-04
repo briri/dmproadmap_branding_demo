4de4625269866bed9efc94cb8f33ea45c56ef82c
Brian Riley <riley.bri@gmail.com>
2017-08-04 14:04:14 -0700

    reverted master back to original generic version

--- link.sh
+++ link.sh
@@ -2,17 +2,6 @@ cd ..
 BASE=`pwd`
 echo "Removing config file(s)"
 rm $BASE/core_roadmap/config/branding.yml
-echo "Removing image file(s)"
-rm $BASE/core_roadmap/app/assets/images/logo.jpg
-echo "Removing stylesheet file(s)"
-rm $BASE/core_roadmap/app/assets/stylesheets/dmproadmap.scss
-rm $BASE/core_roadmap/app/assets/stylesheets/branding-overrides.scss
-echo "Removing branded view file(s)"
-rm -Rf $BASE/core_roadmap/app/views/branded
 
 echo "Linking file(s)"
 ln -s $BASE/dmproadmap_branding_demo/config/branding.yml $BASE/core_roadmap/config/branding.yml
-ln -s $BASE/dmproadmap_branding_demo/app/assets/images/logo.jpg $BASE/core_roadmap/app/assets/images/logo.jpg
-ln -s $BASE/dmproadmap_branding_demo/app/assets/stylesheets/dmproadmap.scss $BASE/core_roadmap/app/assets/stylesheets/dmproadmap.scss
-ln -s $BASE/dmproadmap_branding_demo/app/assets/stylesheets/branding-overrides.scss $BASE/core_roadmap/app/assets/stylesheets/branding-overrides.scss
-ln -s $BASE/dmproadmap_branding_demo/app/views/branded/ $BASE/core_roadmap/app/views/branded
