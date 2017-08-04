b69abfac54bea034542ae9b1cbfd795d37da0ad6
Brian Riley <riley.bri@gmail.com>
2017-08-04 15:23:24 -0700

    added example style

--- link.sh
+++ link.sh
@@ -4,6 +4,7 @@ echo "Removing config file(s)"
 rm $BASE/brand_demo/config/branding.yml
 echo "Removing image file(s)"
 rm $BASE/brand_demo/app/assets/images/logo.jpg
+rm $BASE/brand_demo/app/assets/images/rainbow_wave_long.jpg
 echo "Removing stylesheet file(s)"
 rm $BASE/brand_demo/app/assets/stylesheets/dmproadmap.scss
 rm $BASE/brand_demo/app/assets/stylesheets/branding-overrides.scss
@@ -13,6 +14,7 @@ rm -Rf $BASE/brand_demo/app/views/branded
 echo "Linking file(s)"
 ln -s $BASE/dmproadmap_branding_demo/config/branding.yml $BASE/brand_demo/config/branding.yml
 ln -s $BASE/dmproadmap_branding_demo/app/assets/images/logo.jpg $BASE/brand_demo/app/assets/images/logo.jpg
+ln -s $BASE/dmproadmap_branding_demo/app/assets/images/rainbow_wave_long.jpg $BASE/brand_demo/app/assets/images/rainbow_wave_long.jpg
 ln -s $BASE/dmproadmap_branding_demo/app/assets/stylesheets/dmproadmap.scss $BASE/brand_demo/app/assets/stylesheets/dmproadmap.scss
 ln -s $BASE/dmproadmap_branding_demo/app/assets/stylesheets/branding-overrides.scss $BASE/brand_demo/app/assets/stylesheets/branding-overrides.scss
 ln -s $BASE/dmproadmap_branding_demo/app/views/branded/ $BASE/brand_demo/app/views/branded
