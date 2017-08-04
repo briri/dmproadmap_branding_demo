7eb21482255df3eeb8dca7c4e63364dbc110f04c
Brian Riley <riley.bri@gmail.com>
2017-08-04 15:26:29 -0700

    updated link file

--- link.sh
+++ link.sh
@@ -1,7 +1,11 @@
 cd ..
 BASE=`pwd`
 echo "Removing config file(s)"
-rm $BASE/core_roadmap/config/branding.yml
+rm $BASE/brand_demo/config/branding.yml
+rm $BASE/brand_demo/app/assets/stylesheets/dmproadmap.scss
+rm $BASE/brand_demo/app/assets/stylesheets/branding-overrides.scss
 
 echo "Linking file(s)"
-ln -s $BASE/dmproadmap_branding_demo/config/branding.yml $BASE/core_roadmap/config/branding.yml
+ln -s $BASE/dmproadmap_branding_demo/config/branding.yml $BASE/brand_demo/config/branding.yml
+ln -s $BASE/dmproadmap_branding_demo/app/assets/stylesheets/dmproadmap.scss $BASE/brand_demo/app/assets/stylesheets/dmproadmap.scss
+ln -s $BASE/dmproadmap_branding_demo/app/assets/stylesheets/branding-overrides.scss $BASE/brand_demo/app/assets/stylesheets/branding-overrides.scss
